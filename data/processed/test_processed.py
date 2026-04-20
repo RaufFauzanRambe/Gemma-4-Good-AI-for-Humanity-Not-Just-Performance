b608e96a5738910b7bf700f89c2a56727f6a0fe4
Ra'uf Fauzan Rambe <ramberauffauzan@gmail.com>
2026-04-20 00:19:59 -0700

    Add test_processed.py with sample data

---
+++ data/processed/test_processed.py
@@ -0,0 +1,4 @@
+id,text_clean,text_tokens,label,label_id,length
+1,"renewable energy important future","renewable|energy|important|future",environment,0,4
+2,"digital classrooms growing rapidly","digital|classroom|grow|rapidly",education,1,4
+3,"ai helps early disease detection","ai|help|early|disease|detection",health,2,5
