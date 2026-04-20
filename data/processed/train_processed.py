2fea0502c27ce9a49ddc1c280edb0cfe3ff45c7f
Ra'uf Fauzan Rambe <ramberauffauzan@gmail.com>
2026-04-20 00:19:27 -0700

    Add.test_processed

---
+++ data/processed/train_processed.py
@@ -0,0 +1,7 @@
+id,text_clean,text_tokens,label,label_id,length
+1,"air pollution increasing urban areas","air|pollution|increase|urban|area",environment,0,5
+2,"students lack access quality education","student|lack|access|quality|education",education,1,5
+3,"healthcare systems need better ai support","healthcare|system|need|better|ai|support",health,2,6
+4,"climate change affects global food supply","climate|change|affect|global|food|supply",environment,0,6
+5,"online learning improves accessibility","online|learning|improve|accessibility",education,1,4
+6,"ai assist doctors diagnosis","ai|assist|doctor|diagnosis",health,2,4
